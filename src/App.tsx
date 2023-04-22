884a6f717025715fe17ae1806b4b56d1c20a9fe2
EWA CHWEDCZUK <echwedczuk0@gmail.com>
2023-04-22 16:28:17 +0200

    add main input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import Input from "./Input";
+import { Layout } from "./Layout/index";
+
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">It's Only a ToDo list</h1>
-    </>
+    <Layout>
+      <div className="bg-stone-200 flex-col absolute  m-6 p-6 justify-center justify-items-center content-center rounded-2xl drop-shadow-2xl">
+        <h1 className="text-zinc-700 m-6 text-3xl font-bold ">
+          It's Only a ToDo list
+        </h1>
+        <Input />
+      </div>
+    </Layout>
   );
 }
 
