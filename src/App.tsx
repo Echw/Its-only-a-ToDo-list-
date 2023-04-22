980a965d0c26cc794db5e160d9a0e48e806d7cf0
EWA CHWEDCZUK <echwedczuk0@gmail.com>
2023-04-22 09:15:55 +0200

    congfig tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
 function App() {
   return (
     <>
-      <h1>It's Only a ToDo list</h1>
+      <h1 className="text-3xl font-bold underline">It's Only a ToDo list</h1>
     </>
   );
 }
