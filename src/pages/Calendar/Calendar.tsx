7620b82e11d4aacd2e76a939c4623e1574aad320
EWA CHWEDCZUK <echwedczuk0@gmail.com>
2023-05-09 20:41:16 +0200

    add calendar page

--- src/pages/Calendar/Calendar.tsx
+++ src/pages/Calendar/Calendar.tsx
@@ -1,5 +1,30 @@
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import { useAppContext } from "../../utils/hooks/useAppContext";
+
 const Calendar = () => {
-  return <>Calendar</>;
+  const { tasks } = useAppContext();
+
+  return (
+    <div>
+      <FullCalendar
+        plugins={[dayGridPlugin]}
+        initialView="dayGridMonth"
+        weekends={true}
+        events={tasks}
+        eventContent={renderEventContent}
+      />
+    </div>
+  );
 };
+
+// a custom render function
+function renderEventContent(eventInfo: any) {
+  return (
+    <>
+      <i>{eventInfo.event.title}</i>
+    </>
+  );
+}
 
 export default Calendar;
