9f46d05617761f929f81e0d3616c56db5b06b0ca
EWA CHWEDCZUK <echwedczuk0@gmail.com>
2023-05-10 19:49:50 +0200

    add clear tasks button

--- src/pages/List/List.tsx
+++ src/pages/List/List.tsx
@@ -1,13 +1,38 @@
+import { Button } from "@material-tailwind/react";
 import { DoneList } from "../../DoneList";
 import { TaskInput } from "../../TaskInput";
 import { TaskList } from "../../TaskList";
+import { useAppContext } from "../../utils/hooks/useAppContext";
 
 export const List = () => {
+  const {
+    setTasks,
+    setDoneTasks,
+    saveDoneTasksInLocalStorage,
+    saveTasksInLocalStorage,
+  } = useAppContext();
+
+  const clearTasks = () => {
+    setDoneTasks([]);
+    setTasks([]);
+    saveDoneTasksInLocalStorage([]);
+    saveTasksInLocalStorage([]);
+  };
+
   return (
     <>
       <TaskInput />
       <TaskList />
       <DoneList />
+      <Button
+        onClick={() => {
+          clearTasks();
+        }}
+        fullWidth
+        className="bg-red-400 text-brown-50 rounded-none shadow-none hover:bg-gray-700 hover:shadow-none"
+      >
+        Remove all tasks
+      </Button>
     </>
   );
 };
